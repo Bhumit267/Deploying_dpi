3dc8a0adfa2af40ea356529b1d41cbdcd08c399a
Bhumit267 <bhumitprs15101975@gmail.com>
2025-01-14 14:51:36 +0530

    change_in_serverjs

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000   ;
+const PORT = process.env.PORT || 3000   ;
 
 app.use(express.json());
 
-app.get('/', (req,res)=>{
+app.get('/', (_req,res)=>{
     res.send('API is live !')
 })
 
