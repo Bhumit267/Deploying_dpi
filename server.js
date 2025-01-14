8eeeb0055113a693e36d93e0815711d8600591f5
Bhumit267 <bhumitprs15101975@gmail.com>
2025-01-14 14:42:39 +0530

    added_servejs

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000   ;
+
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('API is live !')
+})
+
+app.post('/api/data', (req,res)=>{
+    const {name,age} = req.body;
+    res.json({message: `Hello ${name}, you are ${age} years old.`})
+
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)    
+});
+
+
+
+
